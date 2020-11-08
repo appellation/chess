fbf6d74bde6cb1daef2291bc61b4e80ea879650f
Will Nelson <will@wnelson.xyz>
2020-11-07 20:50:41 -0800

    Add workflow

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ ! -d "./chess" ]; then
+	git clone https://appellation:$GITHUB_ACCESS_TOKEN@github.com/appellation/chess.git
+	cd chess
+else
+	cd chess
+	git fetch origin
+	git reset --hard origin/master
+fi
+
+mv -f ../.env .
+
+docker-compose pull
+docker-compose build
+docker-compose up -d
