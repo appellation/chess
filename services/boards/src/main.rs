4f2277d7e4e48ede9e81abbad3445aa58b13e999
Will Nelson <will@wnelson.xyz>
2020-11-14 18:48:54 -0800

    Send board URL

--- services/boards/src/main.rs
+++ services/boards/src/main.rs
@@ -11,27 +11,16 @@ async fn handle_request(req: Request<()>) -> Result {
 			.as_str()
 			.try_into()?;
 
-	let accept = req
-		.header("accept")
-		.map(|h| h.last())
-		.and_then(|h| h.as_str().parse::<Mime>().ok());
+	let body = spawn_blocking(move || {
+		let svg: render::Svg = board.try_into()?;
+		Ok::<Vec<u8>, Error>(svg.try_into()?)
+	})
+	.await?;
 
-	match accept.as_ref().map(|mime| mime.essence()) {
-		Some("text/plain") | Some("*/*") => Ok(Body::from_string(board.to_string()).into()),
-		Some("image/png") => {
-			let body = spawn_blocking(move || {
-				let svg: render::Svg = board.try_into()?;
-				Ok::<Vec<u8>, Error>(svg.try_into()?)
-			})
-			.await?;
-
-			let mut res = Response::new(StatusCode::Ok);
-			res.set_body(body);
-			res.set_content_type("image/png");
-			Ok(res)
-		}
-		_ => Ok(Response::new(StatusCode::NotAcceptable)),
-	}
+	let mut res = Response::new(StatusCode::Ok);
+	res.set_body(body);
+	res.set_content_type("image/png");
+	Ok(res)
 }
 
 #[async_std::main]
--- services/bot/bin/index.ts
+++ services/bot/bin/index.ts
@@ -5,6 +5,7 @@ import { Lexer, Parser, Args } from 'lexure';
 import fetch from 'node-fetch';
 
 const apiUrl = process.env.API_URL ?? 'http://localhost:8080';
+const boardsUrl = process.env.BOARDS_URL ?? 'http://localhost:8081';
 const amqpUrl = process.env.AMQP_URL ?? 'localhost';
 const token = process.env.DISCORD_TOKEN;
 if (!token) throw new Error('missing DISCORD_TOKEN');
@@ -55,6 +56,25 @@ broker.on('MESSAGE_CREATE', async (message: Message, { ack }: AmqpResponseOption
 			console.log(gameId);
 			break;
 		}
+		case 'game': {
+			const res = await fetch(`${apiUrl}/games/current`, {
+				method: 'get',
+				headers: {
+					'x-user-id': message.author.id,
+					'x-account-type': 'Discord',
+				},
+			});
+
+			if (!res.ok) {
+				proxy.post(`/channels/${message.channel_id}/messages`, { content: 'no game' }).catch(console.error);
+				return;
+			}
+
+			const game = await res.json();
+			proxy.post(`/channels/${message.channel_id}/messages`, { content: `${boardsUrl}/${game.board}` }).catch(console.error);
+
+			break;
+		}
 		case 'move': {
 			const move = args.single();
 			const res = await fetch(`${apiUrl}/games/current/moves`, {
@@ -68,7 +88,14 @@ broker.on('MESSAGE_CREATE', async (message: Message, { ack }: AmqpResponseOption
 				}),
 			});
 
-			if (!res.ok) proxy.post(`/channels/${message.channel_id}/messages`, { content: 'invalid move' }).catch(console.error);
+			if (!res.ok) {
+				proxy.post(`/channels/${message.channel_id}/messages`, { content: 'invalid move' }).catch(console.error);
+				return;
+			}
+
+			const game = await res.json();
+			proxy.post(`/channels/${message.channel_id}/messages`, { content: `${boardsUrl}/${game.board}` }).catch(console.error);
+
 			break;
 		}
 	}
