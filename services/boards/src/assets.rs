61f508906f13c085c4d7e94c8d2ab06ff803758b
Will Nelson <will@wnelson.xyz>
2020-11-14 23:46:06 -0800

    Update chess assets

--- services/boards/src/assets.rs
+++ services/boards/src/assets.rs
@@ -1,15 +1,15 @@
-pub const BASE_BOARD: &'static [u8; 2492] = include_bytes!("./assets/chess-01.svg");
+pub const BASE_BOARD: &'static [u8; 47060] = include_bytes!("./assets/chess-01.svg");
 
-pub const WHITE_ROOK: &'static [u8; 489] = include_bytes!("./assets/chess-02.svg");
-pub const WHITE_KNIGHT: &'static [u8; 3592] = include_bytes!("./assets/chess-03.svg");
-pub const WHITE_BISHOP: &'static [u8; 3675] = include_bytes!("./assets/chess-04.svg");
-pub const WHITE_KING: &'static [u8; 2547] = include_bytes!("./assets/chess-05.svg");
-pub const WHITE_QUEEN: &'static [u8; 3816] = include_bytes!("./assets/chess-06.svg");
-pub const WHITE_PAWN: &'static [u8; 2403] = include_bytes!("./assets/chess-07.svg");
+pub const WHITE_ROOK: &'static [u8; 1136] = include_bytes!("./assets/chess-02.svg");
+pub const WHITE_KNIGHT: &'static [u8; 3744] = include_bytes!("./assets/chess-03.svg");
+pub const WHITE_BISHOP: &'static [u8; 4069] = include_bytes!("./assets/chess-04.svg");
+pub const WHITE_KING: &'static [u8; 2795] = include_bytes!("./assets/chess-05.svg");
+pub const WHITE_QUEEN: &'static [u8; 4336] = include_bytes!("./assets/chess-06.svg");
+pub const WHITE_PAWN: &'static [u8; 2649] = include_bytes!("./assets/chess-07.svg");
 
-pub const BLACK_ROOK: &'static [u8; 399] = include_bytes!("./assets/chess-08.svg");
-pub const BLACK_KNIGHT: &'static [u8; 2224] = include_bytes!("./assets/chess-09.svg");
-pub const BLACK_BISHOP: &'static [u8; 2749] = include_bytes!("./assets/chess-10.svg");
-pub const BLACK_KING: &'static [u8; 3602] = include_bytes!("./assets/chess-11.svg");
-pub const BLACK_QUEEN: &'static [u8; 3432] = include_bytes!("./assets/chess-12.svg");
-pub const BLACK_PAWN: &'static [u8; 2157] = include_bytes!("./assets/chess-13.svg");
+pub const BLACK_ROOK: &'static [u8; 847] = include_bytes!("./assets/chess-08.svg");
+pub const BLACK_KNIGHT: &'static [u8; 2356] = include_bytes!("./assets/chess-09.svg");
+pub const BLACK_BISHOP: &'static [u8; 3093] = include_bytes!("./assets/chess-10.svg");
+pub const BLACK_KING: &'static [u8; 3899] = include_bytes!("./assets/chess-11.svg");
+pub const BLACK_QUEEN: &'static [u8; 3947] = include_bytes!("./assets/chess-12.svg");
+pub const BLACK_PAWN: &'static [u8; 2401] = include_bytes!("./assets/chess-13.svg");
